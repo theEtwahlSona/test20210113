6bf84951a199648342190376cf0436f357239e5d
sona <1404483052@qq.com>
2021-01-13 17:56:39 +0800

    qwer

--- src/com/hinata/entity/Student.java
+++ src/com/hinata/entity/Student.java
@@ -8,13 +8,15 @@ public class Student {
 
     private Integer id;
     private String name;
+    private String email;
 
     public Student() {
     }
 
-    public Student(Integer id, String name) {
+    public Student(Integer id, String name, String email) {
         this.id = id;
         this.name = name;
+        this.email = email;
     }
 
     public Integer getId() {
@@ -33,11 +35,20 @@ public class Student {
         this.name = name;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     @Override
     public String toString() {
         return "Student{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
                 '}';
     }
 
